a7a2a0e0b20a7e12f3530fd1380f28f0867b42fc
Jesse Raab <jesse.r.raab@gmail.com>
2025-07-30 10:37:08 -0400

    Update run_rna.sh

--- run_rna.sh
+++ run_rna.sh
@@ -1,26 +1,24 @@
-#!/bin/bash
-
-#SBATCH -c 2
-#SBATCH --mem=10G
-#SBATCH -t 24:00:0
-#SBATCH -J NF-RNA
-#SBATCH -o ./logs/rna_pipeline.%a.out
-
-module purge
-module add nextflow
-
-source ~/.secrets/airtable
-
-## MODIFY THE EXPERIMENT ID, WORK, OUTPUT, AND EMAIL FOR YOUR RUN
-nextflow run raab-lab/rnaseq -r main\
-	--sample_sheet /proj/jraablab/users/jraab/menin-mll/full_rna_samples.csv\
-   --single \
+#!/bin/bash	
+	
+#SBATCH -c 2	
+#SBATCH --mem=10G	
+#SBATCH -t 24:00:0	
+#SBATCH -J NF-RNA	
+#SBATCH -o ./logs/rna_pipeline.%a.out	
+	
+module purge	
+module add nextflow	
+	
+#source ~/.secrets/airtable	
+	
+"## MODIFY THE EXPERIMENT ID, WORK, OUTPUT, AND EMAIL FOR YOUR RUN"	
+nextflow run raab-lab/rnaseq -r main\	
+	#NAME?
+   --single \	
 	-w /work/users/j/r/jraab/temp_files/menin-mll/rna   \
-	--outdir /proj/jraablab/users/jraab/menin-mll/data/source_data/rna/\
-	-with-report \
+	#NAME?
+	#NAME?
 	-N jraab@med.unc.edu \
-	-latest \
+	#NAME?
 	-ansi-log false \
-   --profile hg38
-
-
+   --profile hg38	
